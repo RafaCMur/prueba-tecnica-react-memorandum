ddeecba6b64e1a5e78aa6f3259a6192ae5dd74c1
RafaCMur <rafacabanillasm@gmail.com>
2025-03-14 18:02:46 +0100

    feat: make header work on mobile: first approach

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import "../styles/Header.css";
 
 const Header = () => {
   return (
-    <header className="header flex flex-row justify-center">
-      <div className="header-content flex flex-row space-between align-center gap-1">
-        <h1>Header</h1>
+    <header className="header flex flex-row justify-center align-center">
+      <div className="header-content">
+        <h1 className="header-title">Header</h1>
         <div className="flex flex-row space-between align-center gap-1">
           <span>Log in</span>
           <button className="trial-btn">Start your free trial</button>
