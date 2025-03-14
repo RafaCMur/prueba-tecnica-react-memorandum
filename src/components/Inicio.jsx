03409e2a07f887d6cd864c1a724e0674b552bbd8
RafaCMur <rafacabanillasm@gmail.com>
2025-03-14 17:02:17 +0100

    fix: fix styles regarding movies and series buttons

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -11,29 +11,52 @@ const Inicio = () => {
       <Title text="Popular Titles" />
       <section>
         <div className="mt-1 flex flex-row gap-1 flex-wrap">
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer"
-            onClick={() => navigate("/series")}
-          >
-            <img
-              className="placeholder-img"
-              src="/prueba-react/assets/placeholder.png"
-              alt="Logo"
-            />
-            <span>Popular Series</span>
+          <div className="flex flex-col align-center justify-center">
+            <div className="media-parent">
+              <img
+                className="child-img"
+                src="/prueba-react/assets/placeholder.png"
+                alt="Logo"
+              />
+              <div
+                className="child-clickable"
+                onClick={() => navigate("/series")}
+              ></div>
+              <span className="child-text">SERIES</span>
+            </div>
+            <p className="w-100 text-start">Popular Series</p>
           </div>
 
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer"
-            onClick={() => navigate("/peliculas")}
-          >
-            <img
-              className="placeholder-img"
-              src="/prueba-react/assets/placeholder.png"
-              alt="Logo"
-            />
-            <span>Popular Films</span>
+          <div className="flex flex-col align-center justify-center">
+            <div className="media-parent">
+              <img
+                className="child-img"
+                src="/prueba-react/assets/placeholder.png"
+                alt="Logo"
+              />
+              <div
+                className="child-clickable"
+                onClick={() => navigate("/peliculas")}
+              ></div>
+              <span className="child-text">MOVIES</span>
+            </div>
+            <p className="w-100 text-start">Popular Films</p>
           </div>
+
+          {/* <div className="flex flex-col align-center justify-center">
+            <div
+              className="media-card-inicio flex flex-col align-center justify-center"
+              onClick={() => navigate("/peliculas")}
+            >
+              <span className="placeholder-text">MOVIES</span>
+              <img
+                className="placeholder-img"
+                src="/prueba-react/assets/placeholder.png"
+                alt="Logo"
+              />
+            </div>
+            <span>Popular Films</span>
+          </div> */}
         </div>
       </section>
     </>
