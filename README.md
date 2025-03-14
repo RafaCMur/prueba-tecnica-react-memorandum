b8e1122571daca6b59f6e7b5f19e5db8ed4e3b38
RafaCMur <rafacabanillasm@gmail.com>
2025-03-14 10:59:21 +0100

    feat: add header and css

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 He estructurado el contenido de la prueba en la carpeta `public`.
 
+mini-tailwind.css es un archivo css que he copiado de otro proyecto y he modificado para que se ajuste a las necesidades de este proyecto.
+
 ## ¿Cómo decidió las opciones técnicas y arquitectónicas utilizadas como parte de su solución?
 
 ## ¿Hay alguna mejora que pueda hacer en su envío?
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header flex flex-row space-between align-center">
       <h1>Header</h1>
+      <div className="flex flex-row space-between align-center gap-1">
+        <span>Log in</span>
+        <button>Start your free trial</button>
+      </div>
     </header>
   );
 };
